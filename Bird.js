c1d976f565ed192b6c8ec5f0f65eaf0a430a64ae
vedantsaxena <tanmay.saxena@gmail.com>
2020-05-23 09:58:30 +0530

    sixth commit

--- Bird.js
+++ Bird.js
@@ -2,6 +2,8 @@ class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
+    this.smokeImg = loadImage("sprites/smoke.png");
+    this.trajectory = [];
   }
 
   display() {
@@ -9,5 +11,14 @@ class Bird extends BaseClass {
     //this.body.position.y = mouseY;
 
     super.display();
+    if (this.body.position.x>250 && this.body.velocity.x>10){
+    var position = [this.body.position.x,this.body.position.y];
+    this.trajectory.push(position);
+    }
+    for(var i = 0;i<this.trajectory.length;i = i+1){
+    image(this.smokeImg,this.trajectory[i][0],this.trajectory[i][1],0,10);
+    }
+    
+
   }
 }
